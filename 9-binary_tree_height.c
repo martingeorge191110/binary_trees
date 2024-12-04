74296ba77e6b00207413a6a4e0c956311f050f08
Martin George <martingeorge191110@gmail.com>
2024-12-04 16:42:15 +0200

    Task 6 ---> 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,6 +15,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
