79ebf267073307ed9fb7f7da1ac8fb90b82c6db7
M-sharkawy <mostafaeelsharkawy@gmail.com>
2024-12-05 11:53:44 +0200

    task 14 etdits

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -51,12 +51,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t l_height, r_height
+
     if (tree == NULL)
         return (0);
 
-    int l_height = binary_tree_height(tree->left);
-    int r_height = binary_tree_height(tree->right);
-
+    l_height = binary_tree_height(tree->left);
+    r_height = binary_tree_height(tree->right);
 
     return (l_height - r_height);
 }
